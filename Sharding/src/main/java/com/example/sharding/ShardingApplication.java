0c7538afb981874911052ab6d3cba9ea43d3023f
lichengcan <li929825769@163.com>
2023-06-25 21:57:24 +0800

    1

--- Sharding/src/main/java/com/example/sharding/ShardingApplication.java
+++ Sharding/src/main/java/com/example/sharding/ShardingApplication.java
@@ -3,6 +3,9 @@ package com.example.sharding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lichengcan
+ */
 @SpringBootApplication
 public class ShardingApplication {
 
