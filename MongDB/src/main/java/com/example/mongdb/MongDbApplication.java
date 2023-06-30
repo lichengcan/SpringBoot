4dd22395fded718bc3ba734c7a97c9074f4280c1
lichengcan <li929825769@163.com>
2023-06-30 17:33:23 +0800

    MongoDB练习

---
+++ MongDB/src/main/java/com/example/mongdb/MongDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongDbApplication.class, args);
+    }
+
+}
